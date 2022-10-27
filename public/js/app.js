54a9270b6854413eb251e9ee516e6a5cf521d803
AliRazavi <104730401+AliRazaviDeveloper@users.noreply.github.com>
2022-10-27 20:48:06 +0330

    feat: pwa

---
+++ public/js/app.js
@@ -0,0 +1,36 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('./../sw.js')
+    .then((registration) => {
+      console.log('Service worker registration succeeded:', registration)
+    })
+    .catch((err) => {
+      console.log('Service worker registration failed:', err)
+    })
+}
+
+let installPromptEvent
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault()
+  console.log('before install prompt event')
+  installPromptEvent = e
+})
+
+document.querySelector('#banner').addEventListener('click', (e) => {
+  e.preventDefault()
+  console.log(installPromptEvent)
+  if (installPromptEvent) {
+    installPromptEvent.prompt()
+
+    installPromptEvent.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User Accepted')
+      } else {
+        console.log('User dismissed')
+      }
+
+      installPromptEvent = null
+    })
+  }
+})
---
+++ public/sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', (event) => {
+  console.log('installing service worker', event)
+})
+
+self.addEventListener('activate', (event) => {
+  console.log('activating service worker', event)
+  console.log('v1')
+})
+
+self.addEventListener('fetch', (event) => {})
